0f1d57948d5d130bcb75fd98bc640f81c3dcae15
rckani <info@swworkshop.com>
2017-08-16 20:15:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,20 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+
+app.get('/article-1', function (req, res) {
+  res.send('article 1 response');
+});
+
+app.get('/article-2', function (req, res) {
+  res.send('article 2 response');
+});
+
+app.get('/article-3', function (req, res) {
+  res.send('article 3 response');
+});
+
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
